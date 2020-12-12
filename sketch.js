ccdf2c06a2001ed4d8b2a7ebcbd36cdbcebf0882
TheVirus-Limiter <64677380+TheVirus-Limiter@users.noreply.github.com>
2020-12-12 14:01:36 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,53 @@
+var dog, happyDog;
+var foodS, foodStock;
+var database; 
+var count;
+//var author = "rehan";
+
+function preload()
+{
+  dogimg = loadImage("Dog.png");
+  happyDogimg = loadImage("happydog.png");
+}
+
+function setup() {
+  
+	createCanvas(500, 500);
+  dog = createSprite(250,250,20,20);
+  dog.addImage(dogimg);
+  dog.scale = 0.25;
+  database= firebase.database();
+  foodStock = database.ref('food');
+  foodStock.on("value",readStock);
+  count = 20;
+}
+
+function draw() {  
+  background(46,139,87);
+ // if(author === "rehan"){
+   // throw new TypeError("Hello! Welcome to Virtual Pet One by Rehan! This isnt actually an error, so you can dismiss it, but this is how to use the app: Click the Up arrow key(🔼) to feed the dog. You can feed the dog 20 times, as you have 20 food! Thanks! 🚀")    
+     // }
+  if(keyWentDown(UP_ARROW)){
+    writeStock();
+    dog.addImage(happyDogimg);
+  }
+  drawSprites();
+  textSize = 3;
+  fill("black");
+  stroke(2);
+  text("Food Stock : " + count,20,30);
+ 
+}
+function readStock(data){
+  foodS = data.val();
+}
+
+function writeStock(){
+
+  if(count<1){
+    count=0;
+  }else{
+    count=count-1;
+  }
+  
+}
